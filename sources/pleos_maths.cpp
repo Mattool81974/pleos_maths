28658f0884cd77628324e090160729f816f0a111
Matt Mmo <mmattmmo@gmail.com>
2025-01-13 21:58:49 +0100

    Make chosen elements of function analysis working

--- sources/pleos_maths.cpp
+++ sources/pleos_maths.cpp
@@ -64,40 +64,90 @@ namespace pleos {
         // Analyse a function
         if(functions_redaction_analyse()->is_clicked_during_this_frame(GLFW_MOUSE_BUTTON_LEFT)) {
             // Get datas and asserts
+            std::string function_name = std::string("f");
             std::string needed_function = functions_redaction_expression()->text();
             if(needed_function == "") {return;}
             scls::Formula needed_formula = scls::string_to_formula(needed_function);
 
             // Do the redaction
-            std::string function_name = "f";
-            std::string redaction = std::string("Nous avons la fonction ") + function_name + std::string(" tel que ") + function_name + std::string("(") + std::string(") = ") + needed_formula.to_std_string();
+            Function_Studied fs; fs.function_formula = needed_formula; fs.function_name = function_name; fs.function_unknown = std::string("x");
+            std::string redaction = std::string("Nous avons la fonction ") + function_name + std::string(" tel que ") + function_name + std::string("(") + std::string(") = ") + needed_formula.to_std_string() + std::string(".</br></br>");
+
+            // Add the needed arguments
+            std::vector<scls::GUI_Scroller_Choice::GUI_Scroller_Single_Choice>& objects = functions_redaction_elements_chosen()->objects();
+            for(int i = 0;i<static_cast<int>(objects.size());i++) {
+                std::string complete_name = objects[i].name(); std::vector<std::string> cutted = scls::cut_string(complete_name, std::string("-"));
+                std::string type = cutted[0];
+
+                // Analyse the argument
+                if(type == "definition_set") { function_definition_set(fs, redaction); }
+                else if(type == "image") {
+                    scls::Formula needed_value = scls::string_to_formula(reinterpret_cast<scls::GUI_Text_Input*>(objects[i].object()->child_by_name(objects[i].object()->name() + "_input_x"))->text());
+                    function_image(fs, needed_value, redaction);
+                } redaction += std::string("</br></br>");
+            }
+
             functions_redaction()->set_text(redaction);
         }
 
         // Add a chosen element
         if(functions_redaction_elements()->selection_modified()) {
             std::string current_choice = functions_redaction_elements()->currently_selected_objects_during_this_frame()[0].name();
+            functions_redaction_elements()->unselect_object(functions_redaction_elements()->currently_selected_objects()[0]);
+
+            // Creation name
+            std::string final_choice = current_choice;
+            if(current_choice == "image"){
+                final_choice += std::string("-") + std::to_string(functions_redaction_elements_chosen()->count_object_similar("image", "-"));
+            }
 
             // Get the good current choice
-            std::shared_ptr<scls::GUI_Object>* object = functions_redaction_elements_chosen()->add_object(current_choice);
+            std::shared_ptr<scls::GUI_Object>* object = functions_redaction_elements_chosen()->add_object(final_choice);
 
             // Do the good configuration
             if(object != 0) {
+                int needed_height = 30;
                 std::string needed_title = "";
+                // Creation settings
                 if(current_choice == "definition_set"){
-                    object->get()->set_background_color(scls::Color(255, 0, 0));
                     needed_title = std::string("Ensemble de définition");
                 }
                 else if(current_choice == "image"){
-                    object->get()->set_background_color(scls::Color(0, 0, 255));
+                    needed_height = 60;
                     needed_title = std::string("Image");
                 }
 
-                object->get()->set_height_in_pixel(80);
+                final_choice = object->get()->name();
+                object->get()->set_border_width_in_pixel(1);
+                object->get()->set_height_in_pixel(needed_height);
                 functions_redaction_elements_chosen()->place_objects();
 
                 // Create the title
+                std::shared_ptr<scls::GUI_Text> title = *object->get()->new_object<scls::GUI_Text>(final_choice + "_title");
+                title.get()->attach_top_in_parent();
+                title.get()->set_height_in_pixel(30);
+                title.get()->set_width_in_scale(1);
+                title.get()->set_x_in_object_scale(scls::Fraction(1, 2));
+                title.get()->set_text(needed_title);
 
+                // Finalise the creation
+                if(current_choice == "image"){
+                    // Create the title
+                    std::shared_ptr<scls::GUI_Text> title_x = *object->get()->new_object<scls::GUI_Text>(final_choice + "_title_x");
+                    title_x.get()->attach_bottom_of_object_in_parent(title);
+                    title_x.get()->attach_left_in_parent();
+                    title_x.get()->set_height_in_pixel(30);
+                    title_x.get()->set_text(std::string("Pour x = "));
+                    title_x.get()->set_width_in_scale(scls::Fraction(1, 2));
+
+                    // Create the input
+                    std::shared_ptr<scls::GUI_Text_Input> input_x = *object->get()->new_object<scls::GUI_Text_Input>(final_choice + "_input_x");
+                    input_x.get()->attach_bottom_of_object_in_parent(title);
+                    input_x.get()->attach_right_in_parent();
+                    input_x.get()->set_border_width_in_pixel(1);
+                    input_x.get()->set_height_in_pixel(30);
+                    input_x.get()->set_width_in_scale(scls::Fraction(1, 2));
+                }
             }
         }
     }
